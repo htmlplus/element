e5a9359d4602215d82888480ffd770ece996b49d
Masood Abdolian <masood.abdolian@connect2wow.com>
2022-01-09 15:18:32 +0330

    update

--- src/client/utils/update-attribute.ts
+++ src/client/utils/update-attribute.ts
@@ -2,15 +2,7 @@ import { paramCase } from 'change-case';
 
 export const updateAttribute = (node: Element, key: string, value: any): void => {
   key = paramCase(key);
-  if (value === undefined) {
-    node.removeAttribute(key);
-  } else if (value === null) {
-    node.removeAttribute(key);
-  } else if (value === false) {
-    node.removeAttribute(key);
-  } else if (value === true) {
-    node.setAttribute(key, '');
-  } else {
-    node.setAttribute(key, value);
-  }
+  if ([undefined, null, false].includes(value))
+    return node.removeAttribute(key);
+  node.setAttribute(key, value === true ? '' : value);
 };
