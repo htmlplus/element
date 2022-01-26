b7386d2e1b56c90ac339e1060964d14b552c5951
Masood Abdolian <m.abdolian@gmail.com>
2022-01-26 19:59:03 +0330

    run prettier

--- src/client/decorators/attributes.ts
+++ src/client/decorators/attributes.ts
@@ -1,4 +1,3 @@
-// TODO
 import * as CONSTANTS from '../../configs/constants.js';
 import { PlusElement } from '../../types/index.js';
 import { defineMethod, sync } from '../utils/index.js';
@@ -6,14 +5,12 @@ import { defineMethod, sync } from '../utils/index.js';
 export function Attributes() {
   return function (target: PlusElement, propertyKey: PropertyKey) {
     let update;
-
     defineMethod(target, CONSTANTS.TOKEN_LIFECYCLE_CONNECTED, function (instance, callback, args) {
       update = sync(instance, {});
       return callback?.(...args);
     });
-
     defineMethod(target, CONSTANTS.TOKEN_LIFECYCLE_UPDATE, function (instance, callback, args) {
-      update(instance[propertyKey] || {});
+      update(instance[propertyKey]);
       return callback?.(...args);
     });
   };
