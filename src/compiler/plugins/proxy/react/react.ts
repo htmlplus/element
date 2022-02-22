8cab63ee0fb64bda38b8359985dab4dbe02e449e
Masood Abdolian <m.abdolian@gmail.com>
2022-02-22 19:41:33 +0330

    update

--- src/compiler/plugins/proxy/react/react.ts
+++ src/compiler/plugins/proxy/react/react.ts
@@ -8,15 +8,16 @@ export const reactProxy = (options: ReactProxyOptions) => {
   const name = 'reactProxy';
 
   const finish = (global) => {
-    const component = 'templates/src/components/[component]';
+    const component = 'templates/src/components/component*';
+    const config = { cwd: import.meta.url };
     Object.keys(global.contexts).forEach((context) => {
-      renderTemplate(import.meta.url, component)(context);
+      renderTemplate(component, options.dist, config)(context);
     });
     if (false /*dirty*/) {
-      renderTemplate(import.meta.url, 'templates/src/proxy')(global);
+      renderTemplate('templates/src/proxy*', options.dist, config)(global);
       return;
     }
-    renderTemplate(import.meta.url, ['templates/**/*', '!' + component])(global);
+    renderTemplate(['templates/**/*', '!' + component], options.dist, config)(global);
   };
 
   return {
--- src/compiler/utils/render-template.ts
+++ src/compiler/utils/render-template.ts
@@ -1,19 +1,23 @@
+import glob from 'fast-glob';
 import fs from 'fs';
 import handlebars from 'handlebars';
 import path from 'path';
 import url from 'url';
 
-export const renderTemplate = (root: string, pattern: string | Array<string>) => {
-  const patterns = [pattern].flat();
-  // const dirname = path.dirname(url.fileURLToPath(root));
-  // const filePath = path.resolve(dirname, template);
-  // const content = fs.readFileSync(filePath, 'utf8');
-  return handlebars.compile('content');
+export const renderTemplate = (source: string | Array<string>, destination: string, options?) => (context) => {
+  const files: any = glob.sync([source].flat(), {
+    ...(options || {}),
+    // TODO
+    cwd: path.dirname(options?.cwd).replace('file:///', '')
+  });
+  for (const file of files) {
+    debugger;
+    const filePath = path.resolve(path.dirname(options?.cwd).replace('file:///', ''), file);
+    const content = fs.readFileSync(filePath, 'utf8');
+    const t = handlebars.compile(content)(context);
+    if (!fs.existsSync(path.dirname(path.resolve(destination, file)))) {
+      fs.mkdirSync(path.dirname(path.resolve(destination, file)), { recursive: true });
+    }
+    fs.writeFileSync(path.resolve(destination, file).replace('.hbs', ''), t, 'utf8');
+  }
 };
-
-// fs.copyFileSync('templates/proxy.ts.hbs', options.dist + 'proxy.ts');
-// const a = copyTemplate(import.meta.url, 'templates/**/*')(context);
-// if (!fs.existsSync(path.resolve(options.dist, `src`))) {
-//   fs.mkdirSync(path.resolve(options.dist, `src`), { recursive: true });
-// }
-// fs.writeFileSync(path.resolve(options.dist, `src/${context.fileName}.ts`), a, 'utf8');
