19076e832096040413da2da5addfd3cb4c06bd4b
Masood Abdolian <masood.abdolian@connect2wow.com>
2022-02-24 17:24:07 +0330

    update

--- src/compiler/plugins/reactProxy/reactProxy.ts
+++ src/compiler/plugins/reactProxy/reactProxy.ts
@@ -9,16 +9,19 @@ export const reactProxy = (options: ReactProxyOptions) => {
   const name = 'reactProxy';
 
   const finish = (global) => {
+    const contexts = global.contexts;
+
     const config = { cwd: import.meta.url };
+
     const component = 'templates/src/components/[fileName]*';
+
     const pattenrs = isDirectoryEmpty(options.dist) ? ['templates/**/*', `!${component}`] : ['templates/src/proxy*'];
 
     renderTemplate(pattenrs, options.dist, config)(global);
 
-    Object.keys(global.contexts).forEach((key) => {
-      const context = global.contexts[key];
-      renderTemplate(component, options.dist, config)(context);
-    });
+    for (const key in Object.keys(contexts)) {
+      renderTemplate(component, options.dist, config)(contexts[key]);
+    }
   };
 
   return {
