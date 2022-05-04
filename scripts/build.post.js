bf5e28f95894af849471a3634b50da5284a6a05a
Masood Abdolian <m.abdolian@gmail.com>
2022-05-04 17:35:59 +0430

    release 0.3.1

--- scripts/build.post.js
+++ scripts/build.post.js
@@ -1,3 +1,11 @@
 import cpy from 'cpy';
 
-await cpy('src/**/*.hbs', 'dist');
+await cpy(
+    [
+        'src/**/*.hbs',
+        'package.json',
+        'package-lock.json',
+        'README.md'
+    ], 
+    'dist'
+);
