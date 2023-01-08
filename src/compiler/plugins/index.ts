0191ca92b5a54f25d8b0f81782b057328c218920
Masood Abdolian <m.abdolian@gmail.com>
2023-01-08 22:44:34 +0330

    added vscode plugin

--- src/compiler/plugins/index.ts
+++ src/compiler/plugins/index.ts
@@ -9,4 +9,5 @@ export * from './read.js';
 export * from './readme.js';
 export * from './style.js';
 export * from './validate.js';
+export * from './visualStudioCode.js';
 export * from './webTypes.js';
--- src/compiler/plugins/visualStudioCode.ts
+++ src/compiler/plugins/visualStudioCode.ts
@@ -1,9 +1,16 @@
-import { Global, Plugin } from '../../types';
+import { paramCase } from 'change-case';
+import fs from 'fs-extra';
+import path from 'path';
+
+import { Context, Global, Plugin } from '../../types';
+import { getTags, getType, print } from '../utils/index.js';
 
 export const VISUAL_STUDIO_CODE_OPTIONS: Partial<VisualStudioCodeOptions> = {};
 
 export interface VisualStudioCodeOptions {
   destination: string;
+  reference?: (context: Context) => string;
+  transformer?: (context: Context, component: any) => any;
 }
 
 export const visualStudioCode = (options: VisualStudioCodeOptions): Plugin => {
@@ -12,26 +19,90 @@ export const visualStudioCode = (options: VisualStudioCodeOptions): Plugin => {
   options = Object.assign({}, VISUAL_STUDIO_CODE_OPTIONS, options);
 
   const finish = (global: Global) => {
-    // TODO
-    // {
-    //   version: 1.1,
-    //   tags: [
-    //     {
-    //       name: context.componentKey,
-    //       description: {
-    //         kind: 'markdown',
-    //         value: description
-    //       },
-    //       attributes: properties,
-    //       references: [
-    //         {
-    //           name: 'Source code',
-    //           url: `https://github.com/htmlplus/core/tree/main/src/components/${context.directoryName}/${context.fileName}.tsx`
-    //         }
-    //       ]
-    //     }
-    //   ]
-    // };
+    const contexts = global.contexts.sort((a, b) => {
+      return a.componentTag!.toUpperCase() > b.componentTag!.toUpperCase() ? -1 : +1;
+    });
+
+    const json = {
+      $schema: 'TODO',
+      version: 1.1,
+      tags: [] as any[]
+    };
+
+    for (const context of contexts) {
+      const description = getTags(context.class!).find((tag) => !tag.key)?.value;
+
+      const tag = {
+        name: context.componentTag,
+        description: description,
+        attributes: [] as any,
+        references: [
+          {
+            name: 'Source code',
+            url: options.reference?.(context)
+          }
+        ]
+      };
+
+      for (const property of context.classProperties || []) {
+        const attribute = {
+          name: paramCase(property.key['name']),
+          description: getTags(property).find((tag) => !tag.key)?.value,
+          values: [] as any
+        };
+
+        const type = print(
+          getType(context.fileAST!, property.typeAnnotation?.['typeAnnotation'], {
+            directory: context.directoryPath
+          })
+        );
+
+        const sections = type.split('|');
+
+        for (const section of sections) {
+          const trimmed = section.trim();
+
+          if (!trimmed) continue;
+
+          const isBoolean = /bool|boolean|Boolean/.test(trimmed);
+
+          const isNumber = !isNaN(trimmed as any);
+
+          const isString = /^("|'|`)/.test(trimmed);
+
+          if (isBoolean) {
+            attribute.values.push(
+              {
+                name: 'false'
+              },
+              {
+                name: 'true'
+              }
+            );
+          } else if (isNumber) {
+            attribute.values.push({
+              name: trimmed
+            });
+          } else if (isString) {
+            attribute.values.push({
+              name: trimmed.slice(1, -1)
+            });
+          }
+        }
+
+        tag.attributes.push(attribute);
+      }
+
+      const transformed = options.transformer?.(context, tag) || tag;
+
+      json.tags.push(transformed);
+    }
+
+    const dirname = path.dirname(options.destination);
+
+    fs.ensureDirSync(dirname);
+
+    fs.writeJSONSync(options.destination, json, { encoding: 'utf8', spaces: 2 });
   };
 
   return { name, finish };
