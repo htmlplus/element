213b46eacaa9a908237321bdaaa2952b866ef223
Masood Abdolian <m.abdolian@gmail.com>
2024-03-09 00:50:07 +0330

    fix: Fix the HTMLPlusElement interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,1 @@
-export interface HTMLPlusElement {
-  adoptedCallback?(): void;
-  connectedCallback?(): void;
-  constructedCallback?(): void;
-  disconnectedCallback?(): void;
-  loadedCallback?(): void;
-  updateCallback?(states: Map<string, any>): void;
-  updatedCallback?(states: Map<string, any>): void;
-}
+export interface HTMLPlusElement {}
