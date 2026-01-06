80c6e9b8cdcc57d9e02411dc215be2b9e17da088
Masood Abdolian <m.abdolian@gmail.com>
2026-01-06 23:04:51 +0330

    fix: Resolve JSX typing errors

--- dist/jsx-runtime.d.ts
+++ dist/jsx-runtime.d.ts
@@ -1,4 +1,4 @@
-import type { JSX as JSXReact } from '@types/react';
+import type { JSX as JSXReact } from 'react/jsx-runtime';
 
 type WithPart<T> = T & {
 	part?: string;
@@ -8,15 +8,13 @@ type IntrinsicElementsWithPart = {
 	[K in keyof JSXReact.IntrinsicElements]: WithPart<Omit<JSXReact.IntrinsicElements[K], 'ref'>>;
 };
 
-declare global {
-	namespace JSX {
-		interface IntrinsicElements extends IntrinsicElementsWithPart {
-			host: WithPart<JSXReact.IntrinsicElements['div']> & {
-				[key: string]: unknown;
-			};
-			slot: WithPart<JSXReact.IntrinsicElements['slot']> & {
-				onSlotChange?: (event: Event) => void;
-			};
-		}
+declare namespace JSX {
+	interface IntrinsicElements extends IntrinsicElementsWithPart {
+		host: WithPart<JSXReact.IntrinsicElements['div']> & {
+			[key: string]: unknown;
+		};
+		slot: WithPart<JSXReact.IntrinsicElements['slot']> & {
+			onSlotChange?: (event: Event) => void;
+		};
 	}
 }
